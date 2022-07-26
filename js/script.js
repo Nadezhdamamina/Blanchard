f3a40f82f32a8620489fcbddfcff3cbc01f8c39b
Nadezhda <92863141+Nadezhdamamina@users.noreply.github.com>
2022-07-26 22:24:17 +0200

    done

--- js/script.js
+++ js/script.js
@@ -262,4 +262,43 @@
   }
   initValidate();
 
+  (() => {
+    const MOBILE_WIDTH = 970;
+
+    function getWindowWidth() {
+      return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.body.clientWidth,
+        document.documentElement.clientWidth
+      );
+    }
+
+    function scrollToContent(link, isMobile) {
+      if (isMobile && getWindowWidth() > MOBILE_WIDTH) {
+        return;
+      }
+
+      const href = link.getAttribute('href').substring(1);
+      const scrollTarget = document.getElementById(href);
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+      console.log(elementPosition);
+
+      window.scrollBy({
+        top: elementPosition,
+        behavior: 'smooth'
+      });
+    }
+
+    document.querySelectorAll('.js-scroll-link').forEach(link => {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        scrollToContent(this, true);
+      });
+    });
+  })();
+
 })();
